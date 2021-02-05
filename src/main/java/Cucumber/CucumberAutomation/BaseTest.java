88dd84de46c0299807403cecdbb77c56fda2f5ef
Sidhant <sidhantdubey1908@gmail.com>
2021-02-05 13:38:15 -0300

    Latest code

--- src/main/java/Cucumber/CucumberAutomation/BaseTest.java
+++ src/main/java/Cucumber/CucumberAutomation/BaseTest.java
@@ -2,6 +2,8 @@ package Cucumber.CucumberAutomation;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
@@ -13,38 +15,43 @@ import io.cucumber.java.Scenario;
 
 public class BaseTest {
 
-	public static WebDriver driver;	
-	
+	public static WebDriver driver;
+
 	public static WebDriver getDriver() {
-		 
-		 
-		 System.setProperty("webdriver.chrome.driver", "C:\\Users\\91893\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		 driver = new ChromeDriver();
-		 
-		 return driver;
-		
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\91893\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+
+		return driver;
+
 	}
-	
+
 	public static void getScreenShot(Scenario scenario) {
-		
-		//File screenshot =   ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		
-				byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-			     // FileUtils.copyFile(screenshot, new File("target\\screenshots\\src.png"));
-			      
-			      scenario.attach(screenshot, "image/png", "ss");
+
+		// File screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+		byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+		// FileUtils.copyFile(screenshot, new File("target\\screenshots\\src.png"));
+
+		scenario.attach(screenshot, "image/png", "ss");
 	}
-	
-public static String getScreenShot2(Scenario scenario) throws Exception {
-		
-	String strPath = "D:\\CucumberAutomation2\\target\\screenshots\\src.png";
-		File screenshot =   ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-		
-				//byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
-			     FileUtils.copyFile(screenshot, new File("D:\\CucumberAutomation2\\target\\screenshots\\src.png"));
-			      
-			      //scenario.attach(screenshot, "image/png", "ss");
-			     
-			     return strPath;
+
+	public static String getScreenShot2(Scenario scenario) throws Exception {
+
+		String strPath = "D:\\CucumberAutomation2\\target\\screenshots\\src.png";
+		File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String strFilename = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+		File dest = new File("D:\\CucumberAutomation2\\target\\screenshots\\" + strFilename + ".png");
+
+		// byte[] screenshot =
+		// ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+		// FileUtils.copyFile(screenshot, new
+		// File("D:\\CucumberAutomation2\\target\\screenshots\\src.png"));
+		FileUtils.copyFile(screenshot, new File(dest.toString()));
+
+		// scenario.attach(screenshot, "image/png", "ss");
+
+		return dest.toString();
 	}
 }
